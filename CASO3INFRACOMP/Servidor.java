ffa182f7b0b18008b14826c3f9a35b6805f109a0
Sofia Losada <s.losadam@uniandes.edu.co>
2024-11-05 23:59:45 -0500

    corrección socket

--- CASO3INFRACOMP/Servidor.java
+++ CASO3INFRACOMP/Servidor.java
@@ -9,8 +9,19 @@ public class Servidor {
         int port = 8080; // Server port
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Server listening on port " + port);
+        ServerSocket finalServerSocket = serverSocket;
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                if (finalServerSocket != null && !finalServerSocket.isClosed()) {
+                    finalServerSocket.close();
+                    System.out.println("Server socket closed.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
 
-        
+    
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
